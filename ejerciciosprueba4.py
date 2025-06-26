ff09949e1f6234a15922c565c8bf03635fdb92b3
TomasDuoc-UC <to.ortizq@duocuc.cl>
2025-06-26 15:51:00 -0400

    Add files via upload

---
+++ ejerciciosprueba4.py
@@ -0,0 +1,38 @@
+
+listas_usuarios = []
+dicc1 = []
+
+print("---bienvenido al menu---")
+print("1.-ingresar usuario: ")
+print("2.-buscar usuario: ")
+print("3.-eliminar usuario: ")
+print("4.-salir: ")
+
+match(listas_usuarios):
+    case 1:
+        print(f"ingresar nombre{Tomas}")
+        print("ingresar la opcion 1 :")
+    case 2:
+        print(f"genero del usuario{masculino}")
+        print("ingresar la opcion 2: ")
+    case 3: 
+         print(f"contraseña del usuario{Contraseña}")
+    case 4:
+        print(f"eliminar usuario{usuario}")
+        print("ingresar la opcion 3: ")
+    case 5:
+        print("usted saldra del sistema: ")
+        print("ingresar la opcion 4: ")
+    case _:
+        print("opcion no valida: ")
+
+
+    
+
+dicc1 = []
+
+print("---bienvenido al menu---")
+print("1.-ingresar usuario: ")
+print("2.-buscar usuario: ")
+print("3.-eliminar usuario: ")
+print("4.-salir: ")
